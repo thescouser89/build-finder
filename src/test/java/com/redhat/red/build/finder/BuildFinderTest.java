a5386538675d028731259bc98dc2656d0de7f6a9
Nick Cross <ncross@redhat.com>
2017-11-16 20:22:25 +0000

    PRODTASKS-298 Add ability to specify output directory

--- src/test/java/com/redhat/red/build/finder/BuildFinderTest.java
+++ src/test/java/com/redhat/red/build/finder/BuildFinderTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.redhat.red.build.finder;
 
 import org.junit.Rule;
@@ -5,7 +20,9 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +45,28 @@ public class BuildFinderTest {
         List<String> debug = new BufferedReader(new InputStreamReader(p.getInputStream())).lines().filter( s -> s.contains("DEBUG")).collect(Collectors.toList());
         p.waitFor();
 
-        // System.out.println ("Found debug " + debug);
+        System.out.println ("Found debug " + debug);
         assertTrue (debug.size() > 0);
+    }
+
+
+    @Test
+    public void verifyDirectory() throws Exception
+    {
+        File target = new File (TestUtils.resolveFileResource( "./", "" )
+                                          .getParentFile().getParentFile(), "pom.xml" );
+        File folder = temp.newFolder();
+        try
+        {
+            BuildFinder.main( new String [] { "-k", "-o", folder.getAbsolutePath(), target.getAbsolutePath() } );
+        }
+        finally
+        {
+            new File("config.json").delete();
+        }
+
+        File[] f = folder.listFiles();
+        assertTrue( f != null && f.length == 1 );
+        assertTrue( f[0].getName().equals("checksums-md5.json"));
     }
 }
