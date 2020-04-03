ad120ee7a41ebf64444bf2ef67e56d08186617e4
Jakub Bartecek <jbartece@redhat.com>
2020-04-03 15:32:11 +0200

    Switch CLI to use newer PncClient

--- cli/src/main/java/org/jboss/pnc/build/finder/cli/Main.java
+++ cli/src/main/java/org/jboss/pnc/build/finder/cli/Main.java
@@ -63,6 +63,7 @@ import org.jboss.pnc.build.finder.core.Utils;
 import org.jboss.pnc.build.finder.koji.KojiBuild;
 import org.jboss.pnc.build.finder.koji.KojiClientSession;
 import org.jboss.pnc.build.finder.koji.KojiJSONUtils;
+import org.jboss.pnc.build.finder.pnc.client.PncClient;
 import org.jboss.pnc.build.finder.pnc.client.PncClient14;
 import org.jboss.pnc.build.finder.report.BuildStatisticsReport;
 import org.jboss.pnc.build.finder.report.GAVReport;
@@ -724,7 +725,7 @@ public final class Main implements Callable<Void> {
                     analyzer.setChecksums(checksums);
 
                     if (config.getPncURL() != null) {
-                        PncClient14 pncclient = new PncClient14(config);
+                        PncClient14 pncclient = new PncClient(config);
                         finder = new BuildFinder(session, config, analyzer, cacheManager, pncclient);
                     } else {
                         finder = new BuildFinder(session, config, analyzer, cacheManager);
@@ -775,7 +776,7 @@ public final class Main implements Callable<Void> {
                     }
 
                     if (config.getPncURL() != null) {
-                        PncClient14 pncclient = new PncClient14(config);
+                        PncClient14 pncclient = new PncClient(config);
                         finder = new BuildFinder(session, config, analyzer, cacheManager, pncclient);
                     } else {
                         finder = new BuildFinder(session, config, analyzer, cacheManager);
