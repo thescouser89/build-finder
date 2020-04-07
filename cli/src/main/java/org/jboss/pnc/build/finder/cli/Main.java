68cf713bd61870a2f4646a04a5ac4f26fc12cb57
Jakub Bartecek <jbartece@redhat.com>
2020-04-07 11:05:26 +0200

    Initial implementation of CachingPncClient using HashMap

--- cli/src/main/java/org/jboss/pnc/build/finder/cli/Main.java
+++ cli/src/main/java/org/jboss/pnc/build/finder/cli/Main.java
@@ -63,8 +63,8 @@ import org.jboss.pnc.build.finder.core.Utils;
 import org.jboss.pnc.build.finder.koji.KojiBuild;
 import org.jboss.pnc.build.finder.koji.KojiClientSession;
 import org.jboss.pnc.build.finder.koji.KojiJSONUtils;
+import org.jboss.pnc.build.finder.pnc.client.HashMapCachingPncClient;
 import org.jboss.pnc.build.finder.pnc.client.PncClient;
-import org.jboss.pnc.build.finder.pnc.client.PncClientImpl;
 import org.jboss.pnc.build.finder.report.BuildStatisticsReport;
 import org.jboss.pnc.build.finder.report.GAVReport;
 import org.jboss.pnc.build.finder.report.HTMLReport;
@@ -725,7 +725,7 @@ public final class Main implements Callable<Void> {
                     analyzer.setChecksums(checksums);
 
                     if (config.getPncURL() != null) {
-                        PncClient pncclient = new PncClientImpl(config);
+                        PncClient pncclient = new HashMapCachingPncClient(config);
                         finder = new BuildFinder(session, config, analyzer, cacheManager, pncclient);
                     } else {
                         finder = new BuildFinder(session, config, analyzer, cacheManager);
@@ -776,7 +776,7 @@ public final class Main implements Callable<Void> {
                     }
 
                     if (config.getPncURL() != null) {
-                        PncClient pncclient = new PncClientImpl(config);
+                        PncClient pncclient = new HashMapCachingPncClient(config);
                         finder = new BuildFinder(session, config, analyzer, cacheManager, pncclient);
                     } else {
                         finder = new BuildFinder(session, config, analyzer, cacheManager);
---
+++ core/src/main/java/org/jboss/pnc/build/finder/pnc/client/StaticRemoteCollection.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.build.finder.pnc.client;
+
+import org.jboss.pnc.client.RemoteCollection;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ * Static implementation of RemoteCollection, which loads all the remote content and stores it inside the entity
+ *
+ * @author Jakub Bartecek
+ */
+public class StaticRemoteCollection<T> implements RemoteCollection<T> {
+
+    private Collection<T> staticCollection;
+
+    public StaticRemoteCollection(RemoteCollection<T> remoteCollection) {
+        this.staticCollection = Collections.unmodifiableCollection(remoteCollection.getAll());
+    }
+
+    @Override
+    public int size() {
+        return staticCollection.size();
+    }
+
+    @Override
+    public Collection<T> getAll() {
+        return staticCollection;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return staticCollection.iterator();
+    }
+}
